822207a07dc26fd4d83197fbeaf6678e4c19de38
Farhad Faghihi <farhadfaghihi@outlook.com>
2017-01-13 21:06:32 +0330

    Added the BottomSheetDialogFragment implementation

--- app/src/main/java/io/github/farhadfaghihi/MainActivity.java
+++ app/src/main/java/io/github/farhadfaghihi/MainActivity.java
@@ -1,7 +1,10 @@
 package io.github.farhadfaghihi;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnBottomSheetBehavior = (Button)findViewById(R.id.button_bottomsheetlayout) ;
+        btnBottomSheetBehavior.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(MainActivity.this,PersistentBottomSheetActivity.class) ;
+                startActivity(intent);
+            }
+        });
+
+        Button btnBottomSheetFragment = (Button)findViewById(R.id.button_bottomsheetfragment) ;
+        btnBottomSheetFragment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                showBottomSheetFragment();
+            }
+        });
+    }
+
+    private void showBottomSheetFragment(){
+
+        FragmentModalBottomSheet fragmentModalBottomSheet = new FragmentModalBottomSheet();
+        fragmentModalBottomSheet.show(getSupportFragmentManager(),"BottomSheet Fragment");
     }
 }
